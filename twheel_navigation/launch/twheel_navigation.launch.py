ff3912ca724823d117b25831745cff101935a10d
earthphisek <earthand53@gmail.com>
2025-01-23 11:24:39 +0700

    twheel_navigation

---
+++ twheel_navigation/launch/twheel_navigation.launch.py
@@ -0,0 +1,57 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch_ros.actions import Node
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+
+
+def generate_launch_description():
+    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+    map_dir = os.path.join(get_package_share_directory(
+        'twheel_navigation'), 'maps')
+    map_file = LaunchConfiguration('map', default=os.path.join(
+        map_dir, 'map13.yaml'))
+
+    param_dir = os.path.join(get_package_share_directory(
+        'twheel_navigation'), 'config')
+    param_file = LaunchConfiguration(
+        'params', default=os.path.join(param_dir, 'navigation_param.yaml'))
+
+    nav2_launch_file_dir = os.path.join(
+        get_package_share_directory('nav2_bringup'), 'launch')
+    # rviz_config_dir = os.path.join(
+    #     get_package_share_directory('twheel_navigation'), 'rviz')
+    # rviz_config_file = os.path.join(rviz_config_dir, 'navigation.rviz')
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'map',
+            default_value=map_file,
+            description='Full path to map file to load'),
+
+        DeclareLaunchArgument(
+            'params',
+            default_value=param_file,
+            description='Full path to param file to load'),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                [nav2_launch_file_dir, '/bringup_launch.py']),
+            launch_arguments={
+                'map': map_file,
+                'use_sim_time': use_sim_time,
+                'params_file': param_file}.items(),
+        ),
+
+        # Node(
+        #     package='rviz2',
+        #     executable='rviz2',
+        #     name='rviz2',
+        #     arguments=['-d', rviz_config_file],
+        #     parameters=[{'use_sim_time': use_sim_time}],
+        #     output='screen'),
+    ])
